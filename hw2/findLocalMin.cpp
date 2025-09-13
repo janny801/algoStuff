2c18420911afc5077705ee970e43f85a196b9e8c
janny801 <93785820+janny801@users.noreply.github.com>
2025-09-13 18:17:09 -0500

    stf

---
+++ hw2/findLocalMin.cpp
@@ -0,0 +1,19 @@
+//need make into rhythm code
+
+
+#include <iostream> 
+using namespace std; 
+
+
+int findLocalMin(int array[], int arraysize)
+{
+    //func
+}
+
+
+
+int main()
+{
+
+    return 0; 
+}
